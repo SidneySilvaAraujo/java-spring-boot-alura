fe7d396492fbed9cccc7a1ef2efda0df2a823c40
SidneySilvaAraujo <sidneiaraujo219@gmail.com>
2025-12-26 13:48:13 -0300

    adcionando agendamento de consultas

---
+++ CURSOSpringBoot3DocumenteTesteEPrepareUmaAPIParaODeploy/api/src/main/java/med/voll/api/domain/consulta/AgendaDeConsulta.java
@@ -0,0 +1,57 @@
+package med.voll.api.domain.consulta;
+
+import med.voll.api.domain.ValidacaoException;
+import med.voll.api.domain.medico.Medico;
+import med.voll.api.domain.medico.MedicoRepository;
+import med.voll.api.domain.paciente.PacienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AgendaDeConsulta {
+    @Autowired
+    private ConsultaRepository consultaRepository;
+    @Autowired
+    private MedicoRepository medicoRepository;
+    @Autowired
+    private PacienteRepository pacienteRepository;
+
+    public void agendar(DadosAgendamentoConsulta dados) {
+        if (!pacienteRepository.existsById(dados.idPaciente())) {
+            throw new ValidacaoException("Id do paciente informado não existe!");
+        }
+        if (dados.idMedico() != null && !medicoRepository.existsById(dados.idMedico())) {
+            throw new ValidacaoException("Id do médico informado não existe!");
+        }
+
+        var paciente = pacienteRepository.getReferenceById(dados.idPaciente());
+        var medico = escolherMedico(dados);
+        var consulta = new Consulta(null, medico, paciente, dados.data());
+        consultaRepository.save(consulta);
+    }
+
+    private Medico escolherMedico(DadosAgendamentoConsulta dados) {
+        if (dados.idMedico() != null) {
+            return medicoRepository.getReferenceById(dados.idMedico());
+        }
+        if (dados.especialidade() == null) {
+            throw new ValidacaoException("Especialidade é obrigatória quando o médico não for escolhido!");
+        }
+
+        return medicoRepository
+                .escolherMedicoAleatorioLivreNaData(
+                        dados.especialidade(),
+                        dados.data(),
+                        PageRequest.of(0, 1)
+                )
+                .stream()
+                .findFirst()
+                .orElseThrow(() ->
+                        new ValidacaoException(
+                                "não existe médicos disponíveis nessa data!"
+                        )
+                );
+    }
+}
+
--- CURSOSpringBoot3DocumenteTesteEPrepareUmaAPIParaODeploy/api/src/main/java/med/voll/api/domain/medico/MedicoRepository.java
+++ CURSOSpringBoot3DocumenteTesteEPrepareUmaAPIParaODeploy/api/src/main/java/med/voll/api/domain/medico/MedicoRepository.java
@@ -1,7 +1,28 @@
 package med.voll.api.domain.medico;
 
+import med.voll.api.domain.consulta.Consulta;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 public interface MedicoRepository extends JpaRepository<Medico, Long> {
-}
 
+    @Query("""
+        select m from Medico m
+        where m.ativo = true
+          and m.especialidade = :especialidade
+          and m.id not in (
+              select c.medico.id from Consulta c
+              where c.data = :data
+          )
+        order by rand()
+    """)
+    List<Medico> escolherMedicoAleatorioLivreNaData(
+            Especialidade especialidade,
+            LocalDateTime data,
+            Pageable pageable
+    );
+}
