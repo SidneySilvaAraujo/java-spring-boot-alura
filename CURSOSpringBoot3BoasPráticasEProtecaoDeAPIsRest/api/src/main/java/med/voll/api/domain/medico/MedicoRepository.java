e1f52367d852705c9e0fb8d92b6e21fafb436020
SidneySilvaAraujo <sidneiaraujo219@gmail.com>
2026-02-12 07:42:26 -0300

    removendo pastas

--- CURSOSpringBoot3BoasPráticasEProtecaoDeAPIsRest/api/src/main/java/med/voll/api/domain/medico/MedicoRepository.java
+++
@@ -1,7 +0,0 @@
-package med.voll.api.domain.medico;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MedicoRepository extends JpaRepository<Medico, Long> {
-}
-
--- CURSOSpringBoot3DocumenteTesteEPrepareUmaAPIParaODeploy/api/src/main/java/med/voll/api/domain/consulta/MotivoCancelamento.java
+++
@@ -1,8 +0,0 @@
-package med.voll.api.domain.consulta;
-
-public enum MotivoCancelamento {
-
-    PACIENTE_DESISTIL,
-    MEDICO_CANCELOU,
-    OUTROS;
-}
